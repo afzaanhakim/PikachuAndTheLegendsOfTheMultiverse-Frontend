e7bc22265cd44e3ab67fbd48c61dcbc770c0fe88
afzaanhakim <afzaanhakim19@gmail.com>
2021-12-27 12:19:42 -0500

    enabled connectwallet functionality and some html changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'cloak777';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+const [currentAccount, setCurrentAccount] = useState(null); //state to store users wallet
+  const checkIfWalletConnected = async () => {
+try {
+
+  const { ethereum } = window;
+
+if(ethereum) {
+console.log("You have metamask, all good!",  ethereum);
+
+const accounts = await ethereum.request({method: 'eth_accounts'});
+
+if (accounts.length !== 0) {
+  const account = accounts[0];
+  console.log("Found a valid metamask account!", account)
+  setCurrentAccount(account);
+}
+} else {
+console.log("No authorised account here! ")
+}
+}
+catch (error) {
+  console.log("Yikes! We have an error", error)
+}
+  }
+
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (ethereum) {
+        const accounts = await ethereum.request({method:'eth_requestAccounts'})
+        console.log("All the accounts", accounts)
+
+        const account = accounts[0]
+
+        setCurrentAccount(account);
+        console.log("Connected to account", account)
+      }
+    }
+    catch (error) {
+      console.log("error in conencting")
+    }
+  }
+
+
+  useEffect (() => {
+
+    checkIfWalletConnected();
+  }, [])
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">⚔️ Pikachu And The Legends Of The Multiverse ⚔️</p>
+          <p className="sub-text">Legendary Pokemon unite to save the Multiverse from it's creator, Arceus!</p>
           <div className="connect-wallet-container">
             <img
               src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
               alt="Monty Python Gif"
             />
+              <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWallet}
+            >
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
@@ -27,7 +83,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
