c13782be32285b711a055636776e5a5d128253fa
afzaanhakim <afzaanhakim19@gmail.com>
2021-12-29 10:51:48 -0500

    created mintpokemon functions and setup listener for on mint to move to arena

--- src/Components/SelectPokemon/SelectPokemon.js
+++ src/Components/SelectPokemon/SelectPokemon.js
@@ -8,6 +8,17 @@ const SelectPokemon = ({ setPokemonNFT }) => {
   const [pokemon, setPokemon] = useState([]);
   const [pokeContract, setPokeContract] = useState(null);
 
+  const mintPokemonNFTAction = (id) => async () => {
+    try {
+      if (pokeContract) {
+        const mintTxn = await pokeContract.mintPokemonNFT(id);
+        await mintTxn.wait();
+        console.log(mintTxn, "mINT TXN!");
+      }
+    } catch (error) {
+      console.warn("Error while Minting Pokemon Action", error);
+    }
+  };
   useEffect(() => {
     const { ethereum } = window;
 
@@ -19,63 +30,78 @@ const SelectPokemon = ({ setPokemonNFT }) => {
         PokemonNFTGame.abi,
         signer
       );
-      console.log("This is the pokemonNFTGameContract", pokeContract);
       setPokeContract(pokeContract);
-      console.log("this is poke contract state line27", pokeContract);
     } else {
       console.log("Ethereum Object Not Found");
     }
   }, []);
 
   useEffect(() => {
-    console.log("useeffect2");
-    console.log("pokeContract state on line32 - UE2", pokeContract);
     const getPokemon = async () => {
       try {
         console.log("Getting Pokemon to mint");
 
-        console.log("game contract state in useeffect2", pokeContract);
         const pokemonTxn = await pokeContract.getAllDefaultPokemon();
-        console.log("Pokemons", pokemonTxn);
         setPokemon(pokemonTxn);
 
         const pokemon = pokemonTxn.map((pokemonData) => {
-          console.log(
-            "this is transformed pokedata",
-            transformPokemonData(pokemonData)
-          );
           return transformPokemonData(pokemonData);
         });
 
         setPokemon(pokemon);
-        console.log(pokemon, "this is pokemon state on line 41");
       } catch (error) {
         console.error("error is hjere", error);
+      }
+    };
+    const onPokemonMint = async (sender, tokenId, pokemonIndex) => {
+      console.log("PokemonNFTMinted",
+        sender,
+        ":sender",
+        tokenId.toNumber(),
+        ":tokenId",
+        "pokemonIndex:",
+        pokemonIndex.toNumber(),
+        `https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+      );
+      if (pokeContract) {
+        const pokemonNFT = await pokeContract.checkIfUserHasNFT();
+        
+        setPokemonNFT(transformPokemonData(pokemonNFT));
       }
     };
     console.log("what is pokeContract", pokeContract);
     if (pokeContract) {
       getPokemon();
+//SETTING LISTENER FOR NFT MINT
+
+      pokeContract.on('PokemonNFTMinted', onPokemonMint)
+    }
+
+    return () => {
+      if (pokeContract) {
+        //CLEANING UP LISTENER
+        pokeContract.off('PokemonNFTMinted', onPokemonMint);
+      }
     }
   }, [pokeContract]);
 
   const renderPokemon = () => {
-    console.log("what is pokemon in render", pokemon)
-    
-const pp = pokemon.map((poke, index) => <div className="character-item" key={poke.name}>
-<div className="name-container">
-  <p>{poke.name}</p>
-</div>
-<img src={poke.imageURI} alt={poke.name} />
-<br></br>
-<br></br>
-<button
-  type="button"
-  className="character-mint-button"
-  // onClick={mintCharacterNFTAction(index)}
->{`Mint ${poke.name}`}</button>
-</div>)
-    return  pp
+    const pp = pokemon.map((poke, index) => (
+      <div className="character-item" key={poke.name}>
+        <div className="name-container">
+          <p>{poke.name}</p>
+        </div>
+        <img src={poke.imageURI} alt={poke.name} />
+        <br></br>
+        <br></br>
+        <button
+          type="button"
+          className="character-mint-button"
+          onClick={mintPokemonNFTAction(index)}
+        >{`Mint ${poke.name}`}</button>
+      </div>
+    ));
+    return pp;
     // pokemon.map((poke, index) => (
     //   <div className="character-item" key={poke.name}>
     //     <div className="name-container">
@@ -94,7 +120,6 @@ const pp = pokemon.map((poke, index) => <div className="character-item" key={pok
   return (
     <div className="select-character-container">
       <h2> Mint Your Multiverse Legendary to save the universe!</h2>
-      <h1>Lol</h1>
       {pokemon && <div className="character-grid">{renderPokemon()}</div>}
     </div>
   );
